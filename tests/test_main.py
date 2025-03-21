a37a8f6e7f32b19273650bf9c4a4bcf84d47bc44
AydenHocking <hockinga2004@gmail.com>
2025-03-21 12:03:19 -0400

    feat(dir): added tests directory for TDD

---
+++ tests/test_main.py
@@ -0,0 +1,32 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+def test_read_sheep():
+    response = client.get("/sheep/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "name": "Spice",
+        "breed": "Gotland",
+        "sex": "ewe"
+
+    }
+
+def test_add_sheep():
+    response = client.post("/sheep", json={
+        "id": 7,
+        "name": "John",
+        "breed": "Suffolk",
+        "sex": "ewe"
+    })
+    assert response.status_code == 201
+    assert response.json() == {
+        "id": 7,
+        "name": "John",
+        "breed": "Suffolk",
+        "sex": "ewe"
+    }
+
